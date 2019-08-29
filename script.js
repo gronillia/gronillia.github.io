7db5593bc546a1084d851ea36314b47035fba2e7
Glitch (puzzling-shock) <none>
2019-08-29 20:47:19 +0000

    🎰🙎 Updated with Glitch

--- script.js
+++
@@ -1,6 +0,0 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log('hi');
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
